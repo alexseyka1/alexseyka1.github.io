dba1f396fbfc6effd9fedbf8a350b006fb4d4fcf
Oleksii Haidai <oleksii@otark.com>
2026-05-18 16:49:11 +0300

    feat: Extracted menu items to config

---
+++ config/menu.ts
@@ -0,0 +1,12 @@
+type MenuItem = {
+  label: string;
+  href: string;
+};
+
+export const MENU_ITEMS: MenuItem[] = [
+  { label: "Home", href: "/#" },
+  { label: "Contact", href: "/#contact" },
+  { label: "Timeline", href: "/#timeline" },
+  { label: "Projects", href: "/#projects" },
+  { label: "Skills & Tools", href: "/#skills-and-tools" },
+];
