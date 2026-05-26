bcad441f1e6ed897ee29afa961cdbc3e157d60e3
Oleksii Haidai <oleksii@otark.com>
2026-05-26 17:25:06 +0300

    Deployed by Bash script

---
+++ _next/static/WmulfI39Z6ecVRssCnslt/_buildManifest.js
@@ -0,0 +1,34 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/01v4xf27u.ltd.js"
+  ],
+  "/_error": [
+    "static/chunks/0rpxkwoo1e1.o.js"
+  ],
+  "/projects/dok": [
+    "static/chunks/113p9.5p3nvfd.js"
+  ],
+  "/projects/rush-hq": [
+    "static/chunks/16d3um01_eovm.js"
+  ],
+  "/projects/solar-service-group": [
+    "static/chunks/158yktkb1kmw_.js"
+  ],
+  "/timeline": [
+    "static/chunks/0web_jh614pvt.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/projects/dok",
+    "/projects/rush-hq",
+    "/projects/solar-service-group",
+    "/timeline"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/0web_jh614pvt.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/timeline", [
+  "static/chunks/0~ip74j58dhwm.js",
+  "static/chunks/13x41qhrr.cci.js",
+  "static/chunks/0xuxdge0y.b_l.js",
+  "static/chunks/12ghl.19d1b-f.js",
+  "static/chunks/0q6i_k5k~ikmf.js",
+  "static/chunks/turbopack-0mni8qq1buheu.js"
+])
