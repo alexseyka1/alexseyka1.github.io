c8734312cc44824388d2c953c21c53bc12766d6d
Oleksii Haidai <oleksii@otark.com>
2026-06-08 14:48:18 +0300

    Deployed by Bash script

---
+++ _next/static/chunks/0oto55c1br5pl.js
@@ -0,0 +1,10 @@
+__turbopack_load_page_chunks__("/timeline", [
+  "static/chunks/0mt-dheb1y0i_.js",
+  "static/chunks/13x41qhrr.cci.js",
+  "static/chunks/13isidb_qmf2b.js",
+  "static/chunks/0iixo1fmk9d57.js",
+  "static/chunks/0gh~cvpohxbl1.js",
+  "static/chunks/02teu~3dsuw6z.js",
+  "static/chunks/15z82-cw3b532.css",
+  "static/chunks/turbopack-0bi3ru_uja0c..js"
+])
