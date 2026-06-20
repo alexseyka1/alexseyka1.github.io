ec138e87e74bdf3b8053901464cf61a5e33a531f
Oleksii Haidai <oleksii@otark.com>
2026-06-20 14:10:27 +0300

    feat: Added sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://alexseyka1.github.io",
+  generateRobotsTxt: true, // Optional: Generates a robots.txt file too
+  // exclude: ['/protected-page'], // Optional: Exclude specific routes
+};
