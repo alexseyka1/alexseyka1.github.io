7232fba1c76a7749f18192629b28acf487a46fb8
Oleksii Haidai <oleksii@otark.com>
2026-06-25 09:22:10 +0300

    fix: Made a single sitemap and added github actions based deployment

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,5 @@ module.exports = {
   siteUrl: "https://alexseyka1.github.io",
   generateRobotsTxt: true, // Optional: Generates a robots.txt file too
   // exclude: ['/protected-page'], // Optional: Exclude specific routes
+  generateIndexSitemap: false,
 };
